ad60ba625e93cdc1ce6b387bb36ca6047992260e
Bartek412 <bartekg412@gmail.com>
2018-10-05 18:15:30 +0200

    task done

---
+++ 6 CreateElementsInJS/main.js
@@ -0,0 +1,24 @@
+let size = 10;
+let number = 1;
+const btn = document.createElement('button');
+const ul = document.createElement('ul');
+
+const init = () => {
+    btn.textContent = 'Dodaj 10 elementów listy'
+    document.body.appendChild(btn);
+    document.body.appendChild(ul);
+}
+
+const createLiElements = () => {
+    for (i = 0; i < 10; i++) {
+        const li = document.createElement('li');
+        li.textContent = `element nr ${number}`;
+        li.style.fontSize = `${size}px`;
+        ul.appendChild(li);
+        size++;
+        number++;
+    }
+}
+
+init();
+btn.addEventListener('click', createLiElements);
